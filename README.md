a063190e196763019751686c19673df2a210b327
artem.zavyalov <artem.zavyalov@gmail.com>
2022-05-28 17:02:15 -0700

    nice wrapper for sure

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # telegram_bot_base
 base for telegram bot, will try to make all extensions as a separate instances
+
+https://github.com/python-telegram-bot/python-telegram-bot
+pip install python-telegram-bot
+
+# testing first bot
+
+https://github.com/python-telegram-bot/python-telegram-bot/wiki/Extensions-%E2%80%93-Your-first-Bot
---
+++ bot.py
@@ -0,0 +1,37 @@
+import logging
+import argparse
+from telegram import Update #pip install python-telegram-bot -U --pre
+from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, CallbackContext
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.DEBUG
+)
+
+parser = argparse.ArgumentParser("bot")
+parser.add_argument("TOKEN", help="Telegram Token")
+args = parser.parse_args()
+TOKEN = args.TOKEN
+
+async def start(update: Update, context: CallbackContext.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+async def echo(update: Update, context: CallbackContext.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+async def caps(update: Update, context: CallbackContext):
+    text_caps = ' '.join(context.args).upper()
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(TOKEN).build()
+    
+    start_handler = CommandHandler('start', start)
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+    caps_handler = CommandHandler('caps', caps)
+
+    application.add_handler(start_handler)
+    application.add_handler(echo_handler)
+    application.add_handler(caps_handler)
+
+    application.run_polling()
